9449ddfdd698cc4e757ca070fe6299d067fe7cd7
sishen <1730821420@qq.com>
2023-08-12 22:59:55 +0800

    update: compatible eval

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,17 @@ import { parse } from './scripts/contain.js'
 document.getElementById("copy")?.addEventListener('click', () => {
   let editDom = document.getElementById("edit") as HTMLTextAreaElement
   let content = editDom?.value
+  console.log("content", content);
+
   try {
-    let inputJson = new Function(`return ${content}`)()
-    let psrseContent = parse(inputJson)
-    copy(psrseContent, () => message.success("复制成功"))
+    let inputJson = new Function(`return 123`)
+    // let psrseContent = parse(inputJson)
+    console.log("inputJson", inputJson);
+    // console.log("psrseContent", psrseContent);
+    // copy(psrseContent as string, () => message.success("复制成功"))
   } catch (error) {
+    console.log("error", error);
+
     message.error("请输入正确的 JSON 格式")
   }
 })
